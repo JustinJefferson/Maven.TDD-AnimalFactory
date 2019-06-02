cf46d86d4a5348e3b74551de892ac7bbeb132ae3
Justin <justleland@gmail.com>
2019-06-02 12:25:21 -0400

    Finished Dog tests and AnimalFactory tests

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,45 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import java.util.Date;
+import rocks.zipcodewilmington.animals.Dog;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public void createDogTest() {
+        Dog dog1 = AnimalFactory.createDog("Skipper", new Date(12121212));
+
+
+        String expectedName = "Skipper";
+        String actualName = dog1.getName();
+
+        Date expectedDate = new Date(12121212);
+        Date actualDate = dog1.getBirthDate();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedDate, actualDate);
+
+    }
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+    public void createCatTest() {
+        Cat cat = AnimalFactory.createCat("Sir Pudditat", new Date(13131313));
+
+        String expectedName = "Sir Pudditat";
+        String actualName = cat.getName();
+
+        Date expectedDate = new Date(13131313);
+        Date actualDate = cat.getBirthDate();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedDate, actualDate);
+    }
 }
--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -1,20 +1,83 @@
 package rocks.zipcodewilmington;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
+import rocks.zipcodewilmington.animals.Animal;
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
  */
 public class DogTest {
+    private Dog fido;
+
+    @Before
+    public void setDog() {
+        Date birth = new Date(10101);
+        fido = new Dog("Fido", birth, 2048);
+        for(int i = 0; i < 6; i++) {
+            fido.eat(new Food());
+        }
+    }
+
     // TODO - Create tests for `new Dog(String name, Date birthDate, Integer id)`
+    @Test
+    public void nameDogTest() {
+        fido.setName("William");
+        String expected = "William";
+        String actual = fido.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `speak`
+    @Test
+    public void speakDogTest() {
+        String expected = "bark!";
+        String actual = fido.speak();
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `setBirthDate(Date birthDate)`
+    @Test
+    public void birthdateDogTest(){
+        Date newBirth = new Date(15000);
+        fido.setBirthDate(newBirth);
+        Date expected = new Date(15000);
+        Date actual = fido.getBirthDate();
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `void eat(Food food)`
+    @Test
+    public void eatDogTest() {
+        Integer expected = 6;
+        Integer actual = fido.getNumberOfMealsEaten();
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create tests for `Integer getId()`
+    @Test
+    public void idDogTest() {
+        Integer expected = 2048;
+        Integer actual = fido.getId();
+        Assert.assertEquals(expected, actual);
+    }
+
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+    @Test
+    public void animalInheritanceTest() {
+        Assert.assertTrue(fido instanceof Animal);
+    }
+
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    @Test
+    public void mammalInheritanceTest() {
+        Assert.assertTrue(fido instanceof Mammal);
+    }
+
     @Test
     public void setNameTest() {
         // Given (a name exists and a dog exists)
